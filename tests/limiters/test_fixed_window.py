b71bc6f29d855e6f92f4febb4c6b9e218c0908e7
Matt Moore <matt.d.moore@outlook.com>
2024-08-01 00:16:18 +0200

    Fix tests

--- tests/limiters/test_fixed_window.py
+++ tests/limiters/test_fixed_window.py
@@ -3,6 +3,7 @@
 import time
 from datetime import timedelta
 
+from leak_snek.interfaces.values.rate import Rate
 from leak_snek.interfaces.values.rate_limit import RateLimit
 from leak_snek.limiters.fixed_window import FixedWindowLimiter
 from tests.fakes.mutex import FakeMutex
@@ -26,33 +27,33 @@ def test_fixed_window() -> None:
 
 
 def test_fixed_window_exceeded() -> None:
-    """Test that fixed window algorithm starts a new window when period exceeded"""
+    """Test that fixed window algorithm starts a new window when period exceeded."""
     # Given: fixed window limiter allowing 1 operation per second
     key = "test_key"
+    storage: FakeStorage[str] = FakeStorage()
     limiter: FixedWindowLimiter[str] = FixedWindowLimiter(
         rate_limit=RateLimit(operations=1, period=timedelta(seconds=1)),
-        rate_storage=FakeStorage(),
+        rate_storage=storage,
         key_mutex=FakeMutex(),
     )
 
     # When: limit_exceeded is called three times with a pause allowing a new window to start
     # Then: first two limits are not exceeded, third time is exceeded
-    assert not limiter.limit_exceeded(key)
-    time.sleep(1)
+    storage.write(key, Rate(operations=1, updated_at=time.monotonic() - 1))
     assert not limiter.limit_exceeded(key)
     assert limiter.limit_exceeded(key)
 
 
 def test_zero_operations() -> None:
-    """Test that limit is always exceeded when operations allowed per second is zero"""
+    """Test that limit is always exceeded when operations allowed per second is zero."""
     # Given: fixed window limiter allowing 0 operations per second
     key = "test_key"
     limiter: FixedWindowLimiter[str] = FixedWindowLimiter(
-        rate_limit=RateLimit(operations=1, period=timedelta(seconds=1)),
+        rate_limit=RateLimit(operations=0, period=timedelta(seconds=1)),
         rate_storage=FakeStorage(),
         key_mutex=FakeMutex(),
     )
 
     # When: limit_exceeded is called
     # Then: always true
-    assert limiter.limit_exceeded
+    assert limiter.limit_exceeded(key)
